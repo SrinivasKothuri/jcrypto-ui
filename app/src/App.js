aacb9646bfa1fe43c567ca1f6718928fe6428658
Srinivas Kothuri <lssrinivas.kothuri@gmail.com>
2022-06-12 23:28:06 +0530

    Add react-bootstrap package.

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Row className="mx-0">
+			<Button as={Col} variant="primary">Button #1</Button>
+			<Button as={Col} variant="secondary" className="mx-2">Button #2</Button>
+			<Button as={Col} variant="success">Button #3</Button>
+		</Row>
+	);
 }
 
 export default App;
